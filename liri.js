985711a46c319bc52d0da533c70390aa06204b7d
Elaine Cadman <elaine.cadman@gmail.com>
2018-11-25 12:12:07 -0800

    OMDB added

--- liri.js
+++ liri.js
@@ -1,13 +1,40 @@
 require("dotenv").config();
+// Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
+var axios = require("axios");
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 
-var Spotify = require('node-spotify-api');
+var Spotify = require('spotify-web-api-js');
 
-// var Spotify = require('spotify-web-api-js');
-// var s = new Spotify();
-// //s.searchTracks()...
+// spotifyApi.setAccessToken('SPOTIFY_SECRET');
 
+var options = new Spotify({
+    id: "SPOTIFY_ID",
+    secret: "SPOTIFY_SECRET"
+});
+
+// var spotifier = Spotify(options);
+
+var command = process.argv[2];
+var userInput = process.argv[3];
+
+var userInputString = JSON.stringify(userInput);
+
+
+
+if (command === "spotify-this-song") {
+
+}
+
+if(command === "movie-this"){
+
+// Then run a request with axios to the OMDB API with the movie specified
+axios.get("http://www.omdbapi.com/?t=" + userInputString + "&y=&plot=short&apikey=trilogy").then(
+ function(response) {
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  }
+);
+}
 // var spotifyApi = new SpotifyWebApi();
 
 // spotifyApi.setAccessToken('<here_your_access_token>');
