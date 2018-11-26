2030fd63e8d9a37d4250f0d5e534851c86ebffbe
Elaine Cadman <elaine.cadman@gmail.com>
2018-11-25 20:53:17 -0800

    comments

--- liri.js
+++ liri.js
@@ -1,27 +1,21 @@
 require("dotenv").config();
 // Axios package
 var axios = require("axios");
-
-// var spotify = new Spotify(keys.spotify);
+// Spotify package
 var Spotify = require('spotify-web-api-js');
-
+// File System package
 var fs = require('fs');
 
+//Grab user input
 var command = process.argv[2];
 var userInput = process.argv[3];
 var userInputString = JSON.stringify(userInput);
 
-// spotifyApi.setAccessToken('SPOTIFY_SECRET');
-
-var options = new Spotify({
-    id: "SPOTIFY_ID",
-    secret: "SPOTIFY_SECRET"
-});
-
-// var spotifier = Spotify(options);
 
 
-//Bands In Town
+
+
+//BANDS IN TOWN
 if (command === "concert-this"){
 axios.get("https://rest.bandsintown.com/artists/" + userInputString + "/events?app_id=codingbootcamp").then(
     function(response) {
@@ -31,14 +25,14 @@ axios.get("https://rest.bandsintown.com/artists/" + userInputString + "/events?a
     }  
 
 
-if (command === "spotify-this-song") {
 
-}
 
+//MOVIE-THIS
 if(command === "movie-this"){
 // Run a request with axios to the OMDB API with the movie title
 axios.get("http://www.omdbapi.com/?t=" + userInputString + "&y=&plot=short&tomatoes=true&apikey=trilogy").then(
  function(response) {
+     // if response is undefined, return info for "Mr. Nobody"
      if (response.data.Title === undefined){
         axios.get("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy").then(
   function(response) {
@@ -54,6 +48,7 @@ axios.get("http://www.omdbapi.com/?t=" + userInputString + "&y=&plot=short&tomat
   }
 );
      }
+     // Else find the movie the user entered
      else{
     console.log("Movie Title: " + response.data.Title);
     console.log("This movie came out in: " + response.data.Year);
@@ -67,11 +62,27 @@ axios.get("http://www.omdbapi.com/?t=" + userInputString + "&y=&plot=short&tomat
   }
 );
 }
+
+// SPOTIFY
+if (command === "spotify-this-song") {
+
+}
+
+var options = new Spotify({
+    id: "SPOTIFY_ID",
+    secret: "SPOTIFY_SECRET"
+});
+
+// spotifyApi.setAccessToken('SPOTIFY_SECRET');
+// var spotify = new Spotify(keys.spotify);
 // var spotifyApi = new SpotifyWebApi();
 
 // spotifyApi.setAccessToken('<here_your_access_token>');
 
 // spotifyApi.setPromiseImplementation(Q);
+// var spotifier = Spotify(options);
+
+
 
 //DO WHAT IT SAYS:
 
